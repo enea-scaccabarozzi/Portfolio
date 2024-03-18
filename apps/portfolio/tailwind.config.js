bab2c32102d61332a84b93a01322f6c5abd70ac6
enea-scaccabarozzi <me@eneascaccabarozzi.xyz>
2024-03-18 22:05:23 +0100

    chore(portfolio): add support for acernity ui

--- apps/portfolio/tailwind.config.js
+++ apps/portfolio/tailwind.config.js
@@ -1,17 +1,33 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind')
+const { join } = require('path')
+const {
+	default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: [
+		join(
+			__dirname,
+			'{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+		),
+		...createGlobPatternsForDependencies(__dirname),
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [addVariablesForColors],
+}
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+	let allColors = flattenColorPalette(theme('colors'))
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	)
+
+	addBase({
+		':root': newVars,
+	})
+}
